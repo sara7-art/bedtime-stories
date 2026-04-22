63f548b25f0082244d3a76233a94b1c40d40e8eb
Sara Yoda <yodasara700@gmail.com>
2026-04-22 01:33:26 -0700

    initial commit

--- src/main/java/com/pluralsight/BedtimeStories.java
+++ src/main/java/com/pluralsight/BedtimeStories.java
@@ -1,4 +1,37 @@
 package com.pluralsight;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Scanner;
 
 public class BedtimeStories {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter the story file name (example: Goldilocks.txt): ");
+        String fileName = scanner.nextLine();
+
+        int lineNumber = 1;
+
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader(fileName));
+
+            String line;
+
+            while ((line = reader.readLine()) != null) {
+                System.out.println(lineNumber + ": " + line);
+                lineNumber++;
+            }
+
+            reader.close();
+
+        } catch (IOException e) {
+            System.out.println("Error: File not found or cannot be read.");
+        }
+
+        scanner.close();
+
+    }
+
 }
